f977ca763db3faec39f9de944601012d92dfb56e
Anik Majumder <101924875+Anik-64@users.noreply.github.com>
2026-03-12 11:16:36 +0600

    Update routers.js

--- routers/routers.js
+++ routers/routers.js
@@ -1,5 +1,16 @@
 // ALL Routers 
 module.exports = {
     profilePicRouter: require('../server/pictureUpload'),
-    currencyRouter: require('../server/currency/currency')
+    currencyRouter: require('../server/currency/currency'),
+    bookUploadRouter: require('../server/bookUpload').bookUploadRouter,
+    skillsRouter: require('../server/settings/skills'),
+    experiencesRouter: require('../server/settings/experiences'),
+    projectsRouter: require('../server/settings/projects'),
+    projectImagesRouter: require('../server/settings/project_images'),
+    certificationsRouter: require('../server/settings/certifications'),
+    publicationsRouter: require('../server/settings/publications'),
+    educationRouter: require('../server/settings/education'),
+    trainingsRouter: require('../server/settings/trainings'),
+    contactMessagesRouter: require('../server/settings/contact_messages'),
+    auditLogRouter: require('../server/settings/audit_log')
 };
